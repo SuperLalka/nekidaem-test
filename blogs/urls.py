6761e3a5d46868e9394eb4c08a7df253fdcb4b94
Mark Lysenko <oomarkizoo@mail.ru>
2020-11-28 11:28:48 +0300

    Creates urlpatterns for blogs & news feed of users

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from django.urls import include
+from django.views.generic import RedirectView
 
 from rest_framework.routers import DefaultRouter
 from . import views
@@ -10,7 +11,11 @@ routerAPI.register(r'blogs', views.BlogsViewSet, basename='blogs')
 routerAPI.register(r'posts', views.PostsViewSet, basename='posts')
 
 
-app_name = 'custom_requests'
+app_name = 'blogs'
 urlpatterns = [
     url(r'^api/', include(routerAPI.urls)),
+    url(r'^blog/(?P<blog_id>\d+)', views.UserBlogDetailView.as_view(), name='blog'),
+    url(r'^news_feed/(?P<blog_id>\d+)', views.NewsFeedDetailView.as_view(), name='news_feed'),
+    url(r'^user_blog/', views.user_blog, name='user_blog'),
+    url(r'^$', RedirectView.as_view(url='user_blog/', permanent=True)),
 ]
