5892c7fe1832a0b57340ac787fd690b43b3586de
Mark Lysenko <oomarkizoo@mail.ru>
2021-01-07 17:59:50 +0300

    Defines a new default user model in the project settings

--- nekidaem-test/nekidaem_test/settings.py
+++ nekidaem-test/nekidaem_test/settings.py
@@ -97,6 +97,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'blogs.User'
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -143,7 +145,7 @@ SIMPLE_JWT = {
     'AUDIENCE': None,
     'ISSUER': None,
 
-    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_TYPES': ('JWT',),
     'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
     'USER_ID_FIELD': 'id',
     'USER_ID_CLAIM': 'user_id',
