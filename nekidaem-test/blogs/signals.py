e0a9c73ce8a415680e384cdced7f44c4611dcce0
Mark Lysenko <oomarkizoo@mail.ru>
2021-01-07 18:01:40 +0300

    Corrects the logic of the signal for creating a new blog

--- nekidaem-test/blogs/signals.py
+++ nekidaem-test/blogs/signals.py
@@ -3,29 +3,29 @@ from django.core.mail import send_mail
 from django.db.models.signals import post_save
 from django.dispatch.dispatcher import receiver
 
-from blogs import models
+from blogs.models import (
+    Blog, Post, User
+)
 from nekidaem_test import settings
 
 
 @receiver(post_save, sender=User)
-def create_extending_user(instance, **kwargs):
-    if not models.ExtendingUser.objects.filter(user=instance).exists():
-        extending_user = models.ExtendingUser.objects.create(user=instance)
-        extending_user.save()
-        user_blog = models.Blog.objects.create(user=extending_user)
+def create_user_blog(instance, **kwargs):
+    if not Blog.objects.filter(author=instance).exists():
+        user_blog = Blog.objects.create(author=instance)
         user_blog.save()
 
 
-@receiver(post_save, sender=models.Post)
+@receiver(post_save, sender=Post)
 def inform_the_subscriber(instance, **kwargs):
-    list_of_subscriptions = list(models.ExtendingUser.objects.filter(
+    list_of_subscriptions = list(User.objects.filter(
         subscribed_to=instance.blog))
-    for extendinguser in list_of_subscriptions:
-        message = f"Hello {extendinguser.user.username}, " \
+    for user in list_of_subscriptions:
+        message = f"Hello {user.user.username}, " \
                   f"a new post {instance.header} has appeared on the {instance.blog} blog, " \
                   f"you can read it at this link http://{settings.ALLOWED_HOSTS[0]}:{settings.ALLOWED_PORT}/posts/{instance.id}/ " \
                   f"you received this email because you are subscribed to this blog"
-        send_mail(extendinguser.user.username,
+        send_mail(user.user.username,
                   message,
                   settings.PROJECT_EMAIL,
-                  [extendinguser.user.email])
+                  [user.user.email])
